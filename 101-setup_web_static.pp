5b04f0759fac9493298659e9011a759e526214d5
Okoli Ekene <khennyphresh@gmail.com>
2022-09-29 12:29:17 +0100

    Update 101-setup_web_static.pp

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -40,6 +40,11 @@ exec {'create index.html':
   provider => shell,
 }
 
+exec { 'remove-current':
+  command  => 'rm -rf /data/web_static/current',
+  provider => shell,
+}
+
 # change owner of folder /date recursively
 exec {'chown-data':
   command  => 'sudo chown -hR ubuntu:ubuntu /data; sudo chown -hR ubuntu:ubuntu /data/web_static/',
