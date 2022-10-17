bad23f54c14d55f474eca2199629e6307819fce7
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:45:54 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from os import getenv
+from models import storage
 from models.city import City
 from models.review import Review
 from models.place import Place
