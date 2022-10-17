ce6b91b219af7c67d4e0f6c5b7fc872880bca809
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:36:47 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an instance of the Storage will be used"""
-
+"""This module instantiates an object of class FileStorage"""
 from os import getenv
 from models.city import City
 from models.review import Review
@@ -10,13 +9,13 @@ from models.amenity import Amenity
 from models.user import User
 from models.state import State
 
-storage_type = getenv('HBNB_TYPE_STORAGE')
-
-if storage_type == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
+
     storage = DBStorage()
+    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
-    storage = FileStorage()
 
-storage.reload()
+    storage = FileStorage()
+    storage.reload()
