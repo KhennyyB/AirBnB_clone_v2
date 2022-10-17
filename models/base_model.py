3ce24a2705f3b250295ffb5bb375bbfbb5ac0e9c
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:01:18 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DATETIME
-from models import storage_type
+from models import storage
 
 Base = declarative_base()
 
