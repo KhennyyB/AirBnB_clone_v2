7b3486293c4918e049403bb186d7e68ae8574cdb
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:47:26 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage
 from models.place import place_amenity
 from sqlalchemy import Column, String
 
