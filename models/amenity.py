f10fb2d579437cee536b4ff0d5bc2bf52ad91952
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 10:35:49 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -8,8 +8,13 @@ from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    '''amenity class'''
+    """
+        Amenity class
+    """
     __tablename__ = 'amenities'
+
     name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity,
+
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        place_amenities = relationship('Place', secondary=place_amenity,
                                        back_populates='amenities')
