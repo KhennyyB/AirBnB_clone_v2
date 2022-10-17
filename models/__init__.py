4ac4f78edcb347393c4a011975657cbeba925659
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:23:17 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,13 @@
 """This module instantiates an instance of the Storage will be used"""
 
 from os import getenv
+from models.city import City
+from models.review import Review
+from models.place import Place
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.user import User
+from models.state import State
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
