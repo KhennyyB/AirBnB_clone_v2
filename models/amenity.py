fc5eba107cfb46c6f64aacbbb2e794420b64e683
Okoli Ekene <khennyphresh@gmail.com>
2022-10-17 09:51:52 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, String
 class Amenity(BaseModel, Base):
     '''amenity class'''
     __tablename__ = 'amenities'
-    if storage_type == 'db':
+    if storage == 'db':
         name = Column(String(128), nullable=False)
     else:
         name = ""
